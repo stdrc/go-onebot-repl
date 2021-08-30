d69e0a3daf2020c6bea459a0f414a6a232713880
Richard Chien <richardchienthebest@gmail.com>
2021-08-30 23:22:15 +0800

    basic function ok

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module onebot-dummy
 go 1.16
 
 require (
-	github.com/botuniverse/go-libonebot v0.0.0-20210830071014-5a2c0b786a31
+	github.com/botuniverse/go-libonebot v0.0.0-20210830103342-8911de25cadd
 	github.com/sirupsen/logrus v1.8.1
+	github.com/spf13/viper v1.8.1
 )
 
 replace github.com/botuniverse/go-libonebot => ../botuniverse/go-libonebot
--- main.go
+++ main.go
@@ -1,128 +1,94 @@
 package main
 
 import (
-	"encoding/json"
+	"bufio"
+	_ "embed"
+	"fmt"
+	"os"
 	"strings"
-	"time"
 
 	libob "github.com/botuniverse/go-libonebot"
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 type OneBotDummy struct {
 	*libob.OneBot
-	config *Config
+	config        *DummyConfig
+	lastMessageID uint64
 }
 
 type Config struct {
-	libob.Config
-	UserID string
+	OneBot libob.Config `mapstructure:",squash"`
+	Dummy  DummyConfig
 }
 
-func (ob *OneBotDummy) handleGetSelfInfo(w libob.ResponseWriter, r *libob.Request) {
-	w.WriteData(map[string]interface{}{
-		"user_id": ob.config.UserID,
-	})
+type DummyConfig struct {
+	SelfID string `mapstructure:"self_id"`
+	UserID string `mapstructure:"user_id"`
+}
+
+//go:embed default_config.toml
+var defaultConfigString string
+
+func loadConfig() *Config {
+	v := viper.New()
+	v.SetConfigType("toml")
+	v.ReadConfig(strings.NewReader(defaultConfigString))
+	v.SetConfigFile("config.toml")
+	err := v.MergeInConfig()
+	if err != nil && os.IsNotExist(err) {
+		fmt.Println("配置文件不存在, 正在写入默认配置到 config.toml")
+		v.WriteConfigAs("config.toml")
+	}
+	config := &Config{}
+	v.Unmarshal(config)
+	fmt.Printf("配置加载成功: %+v\n", config)
+	return config
 }
 
 func main() {
-	configString := `
-	{
-		"Heartbeat": {
-			"Enabled": false,
-			"Interval": 10
-		},
-		"Auth": {
-			"AccessToken": "abc"
-		},
-		"CommMethods": {
-			"HTTP1": [
-				{
-					"Host": "127.0.0.1",
-					"Port": 5700
-				},
-				{
-					"Host": "127.0.0.1",
-					"Port": 5701
-				}
-			],
-			"HTTPWebhook1": [
-				{
-					"URL": "http://127.0.0.1:8080/"
-				}
-			],
-			"WS": [
-				{
-					"Host": "127.0.0.1",
-					"Port": 6700
-				}
-			],
-			"WSReverse": [
-				{
-					"URL": "ws://127.0.0.1:8080/ws"
-				}
-			]
-		},
-		"UserID": "123"
+	config := loadConfig()
+	ob := &OneBotDummy{
+		OneBot:        libob.NewOneBot("dummy", &config.OneBot),
+		config:        &config.Dummy,
+		lastMessageID: 0,
 	}
-	`
-
-	config := &Config{}
-	json.NewDecoder(strings.NewReader(configString)).Decode(config)
-
-	obdummy := &OneBotDummy{
-		OneBot: libob.NewOneBot("dummy", &config.Config),
-		config: config,
+	logFile, err := os.OpenFile("dummy.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
 	}
-	obdummy.Logger.SetLevel(log.DebugLevel)
+	ob.Logger.SetOutput(logFile)
+	ob.Logger.SetLevel(logrus.InfoLevel)
 
 	mux := libob.NewActionMux()
-	mux.HandleFunc(libob.ActionGetSelfInfo, obdummy.handleGetSelfInfo)
-	mux.HandleFunc(libob.ActionGetVersion, func(w libob.ResponseWriter, r *libob.Request) {
-		w.WriteData(map[string]string{
-			"version":         "1.0.0",
-			"onebot_standard": "v12",
+	mux.HandleFunc(libob.ActionGetVersion, ob.handleGetVersion)
+	mux.HandleFunc(libob.ActionGetSelfInfo, ob.handleGetSelfInfo)
+	mux.HandleFunc(libob.ActionSendMessage, ob.handleSendMessage)
+	mux.HandleFuncExtended("test", func(w libob.ResponseWriter, r *libob.Request) {
+		w.WriteData("It works!")
+	})
+
+	ob.Handle(mux)
+	go ob.Run()
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("请开始对话 (输入 exit 退出):")
+	for {
+		text, _ := reader.ReadString('\n')
+		text = strings.TrimSpace(text)
+		if text == "exit" {
+			ob.Shutdown()
+			break
+		}
+		go ob.Push(&libob.MessageEvent{
+			Event: libob.Event{
+				SelfID:     ob.config.SelfID,
+				Type:       libob.EventTypeMessage,
+				DetailType: "private",
+			},
+			UserID:  ob.config.UserID,
+			Message: libob.Message{libob.TextSegment(text)},
 		})
-	})
-	mux.HandleFunc(libob.ActionSendMessage, func(w libob.ResponseWriter, r *libob.Request) {
-		p := libob.NewParamGetter(w, r)
-		userID, ok := p.GetString("user_id")
-		if !ok {
-			return
-		}
-		msg, ok := p.GetMessage("message")
-		if !ok {
-			return
-		}
-		log.Debugf("Send message: %#v, to %v", msg, userID)
-		w.WriteData(msg)
-	})
-	mux.HandleFuncExtended("do_something", func(w libob.ResponseWriter, r *libob.Request) {
-	})
-
-	obdummy.Handle(mux)
-
-	go func() {
-		for {
-			obdummy.Push(
-				&libob.MessageEvent{
-					Event: libob.Event{
-						SelfID:     "123",
-						Type:       libob.EventTypeMessage,
-						DetailType: "private",
-					},
-					UserID:  "234",
-					Message: libob.Message{libob.TextSegment("hello")},
-				},
-			)
-			time.Sleep(time.Duration(3) * time.Second)
-		}
-	}()
-
-	// go func() {
-	// 	time.Sleep(time.Duration(5) * time.Second)
-	// 	obdummy.Shutdown()
-	// }()
-
-	obdummy.Run()
+	}
 }
